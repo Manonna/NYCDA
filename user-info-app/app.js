878198f0e338616bb645fc8098fb5f831672f4e1
Manon Vrugt <manonvrugt@gmail.com>
2016-10-19 15:40:11 +0200

    searchresult displays users w matching first or lastname

--- user-info-app/app.js
+++ user-info-app/app.js
@@ -8,11 +8,14 @@ const app	 	 =	express()
 app.set ( 'view engine', 'pug' )
 app.set ( 'views', __dirname + '/views' )
 
+let urlencodedParser = bodyParser.urlencoded({ extended: true })
+let jsonParser 		 = bodyParser.json()
+
 app.get( '/users', (req, res) =>{
 	console.log ('rendering....')
 	fs.readFile(__dirname + '/users.json', (err, data) => {
 		if (err) throw err
-		let parsedData = JSON.parse(data)
+			let parsedData = JSON.parse(data)
 		console.log(parsedData)
 		res.render('index', {data: parsedData})
 	})
@@ -20,6 +23,25 @@ app.get( '/users', (req, res) =>{
 
 app.get( '/search', (req, res) =>{
 	res.render('search')
+})
+
+app.post( '/search', urlencodedParser, (req, res) => {
+	console.log("someone searched for " + "\"" + req.body.name + "\"")
+	//res.render('result', {name: req.body.name})
+	let searchResult = []
+	fs.readFile(__dirname + '/users.json', (err, data) => {
+		if (err) throw err
+			let parsedData = JSON.parse(data)
+
+		for (var i = parsedData.length - 1; i >= 0; i--) {
+			if (parsedData[i].firstname == req.body.name || parsedData[i].lastname == req.body.name) {
+				searchResult.push (parsedData[i])
+				res.render('result', {result: searchResult})
+				//console.log(searchResult)
+			}
+		}
+
+	})
 })
 
 app.listen(8000, () => {
