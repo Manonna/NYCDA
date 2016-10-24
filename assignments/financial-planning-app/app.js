ff907fbb6efae2e7f68a16a9148008076ad4222b
Manon Vrugt <manonvrugt@gmail.com>
2016-10-24 14:34:55 +0200

    installed and saved pug, added jquery to search.pug

--- assignments/financial-planning-app/app.js
+++ assignments/financial-planning-app/app.js
@@ -1,10 +1,13 @@
 'use strict'
 // npm init, npm install -- save express, pug, body-parser 
 //Require necessary modules
-const express = require ( 'express' )
-const fs	  = require ( 'fs' )
+const express 	 = require ( 'express' )
+const fs	  	 = require ( 'fs' )
+const bodyparser = require( 'body-parser')
 
-const app	  = express ( )
+const app	     = express ( )
+
+let urlencodedParser = bodyParser.urlencoded({ extended: true })
 
 //define the location of static files to use
 app.use(express.static(__dirname + '/static'))
@@ -19,6 +22,9 @@ app.get ( '/index', (req, res) => {
 	res.render ( 'index' )
 })
 
+app.post ( '/index', (req, res) => {
+	res.render ( 'result' )
+})
 
 
 app.listen (8000, () => {
